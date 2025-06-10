9a9aa4337ca5fbed30b0f74a8221661c8b6e0f81
adenafil <nafilie9@gmail.com>
2025-06-10 21:42:26 +0700

    remove dismiss link

--- public/sw.js
+++ public/sw.js
@@ -34,7 +34,8 @@ self.addEventListener("notificationclick", function (event) {
     event.waitUntil(clients.openWindow(event.notification.data.url));
   } else if (event.action === "dismiss") {
     // Handle dismiss action, if needed
-    event.waitUntil(clients.openWindow('https://elohim.id/wp-content/uploads/2022/02/Ayo-lawan-rasa-malas.png'));
+    // event.waitUntil(clients.openWindow('https://elohim.id/wp-content/uploads/2022/02/Ayo-lawan-rasa-malas.png'));
+    return;
   } else {
     // Default click action
     event.waitUntil(clients.openWindow("/dashboard"));
