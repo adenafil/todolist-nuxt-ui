8be08fb8059c217655d885400a65db2cbf18242d
adenafil <nafilie9@gmail.com>
2025-07-08 21:41:56 +0700

    update name

--- components/landing/Footer.vue
+++ components/landing/Footer.vue
@@ -1,12 +1,12 @@
 <template>
     <!-- Small Footer -->
     <p class="mt-12 text-sm text-gray-500 dark:text-gray-400">
-        Already using Task Manager on another device?
+        Already using Taskify on another device?
         <UButton to="/auth?mode=login" variant="link" color="primary" class="px-0">
             Sign in now
         </UButton>
     </p>
     <p class="mt-2 text-sm text-gray-500 dark:text-gray-400">
-        &copy; {{ new Date().getFullYear() }} Task Manager. All rights reserved.
+        &copy; {{ new Date().getFullYear() }} Taskify. All rights reserved.
     </p>
 </template>
